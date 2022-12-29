e3ff00df88c660643fa549a51e57336cd8470146
Joshua David Gardner <76405317+jgcoding2020@users.noreply.github.com>
2022-12-28 22:06:27 -0500

    Add files via upload

---
+++ Project_5/script.js
@@ -0,0 +1,21 @@
+$("body").css({ 
+  "background": "url(background.jpg) no-repeat center center fixed", 
+  "-webkit-background-size": "cover",
+  "-moz-background-size": "cover",
+  "-o-background-size": "cover",
+  "background-size": "cover",
+});
+
+$("h1").css({
+    "font-size": "200%",
+    "color": "white",
+});
+
+$("header").css({
+    "font-size": "300%",
+    "color": "white",
+});
+
+$("em").css({
+    "font-size": "200%",
+});
